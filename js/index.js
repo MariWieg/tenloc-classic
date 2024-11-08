4c9f77b7071149d09e238a03ce852eef9c54e9c2
Marina <marina.wieg@gmail.com>
2024-11-08 10:04:34 -0600

    js

--- js/index.js
+++ js/index.js
@@ -10,3 +10,34 @@ headerBtn.addEventListener('mouseenter', () => {
 headerBtn.addEventListener('mouseleave', () => {
     headerTooltip.style.display = 'none';
 });
+
+
+// const burgerNav = document.getElementById('burger-nav');
+// const burgerTooltip = document.getElementById('burger-sidenav');
+
+// burgerNav.addEventListener('mouseenter', () => {
+//     burgerTooltip.style.display = 'block';
+// });
+
+// burgerNav.addEventListener('mouseleave', () => {
+//     burgerTooltip.style.display = 'none';
+// });
+
+const burgerNav = document.getElementById('burger-nav');
+const burgerSidenav = document.getElementById('burger-sidenav');
+const burgerSidenavClose = document.getElementById('burger-sidenav-close');
+console.log(burgerSidenav)
+function openNav() {
+    // burgerSidenav.classList.add('open');
+    burgerSidenav.style.display = 'block';
+}
+
+function closeNav() {
+    console.log('closeNav');
+    //burgerSidenav.classList.remove('open');
+    burgerSidenav.style.display = 'none';
+
+}
+
+burgerNav.addEventListener('click', openNav);
+burgerSidenavClose.addEventListener('click', closeNav);
